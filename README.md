c157aac7441063de63e54175d144d534f2f9e1ee
Andrew J. Pierce <andrewp@spaceflightindustries.com>
2019-12-08 16:03:06 -0500

    Update README with setup, structure, and running info;
    Add the -main function to day01 so we can use the `lein run` command.

--- README.md
+++ README.md
@@ -1,5 +1,29 @@
-# advent-2019
+# advent-2019 ![](https://github.com/ajpierce/advent-2019/workflows/Clojure%20CI/badge.svg)
 
-![](https://github.com/ajpierce/advent-2019/workflows/Clojure%20CI/badge.svg)
+Once a year, we dust off our Clojure skills for a week or so before real life gets in the way :)
 
-Once a year, we dust off our Clojure skills for a week or so and then real life gets in the way
+## Setup
+1. Make sure you have [Java](https://adoptopenjdk.net/index.html?variant=openjdk13&jvmVariant=hotspot) installed.
+1. Make sure you have [Clojure](https://clojure.org/guides/getting_started) installed.
+1. Make sure you have [Leiningen](https://leiningen.org/) installed.
+
+## Project Structure
+Puzzle input lives in the `resources/` folder. Each day has its own input file.
+
+Each question has multiple sample inputs with solutions. These examples are used as unit tests. Tests live in the `test/` folder.
+
+Puzzle solutions and supporting functions live in the `src/` folder.
+
++ Common functions that may be used in multiple solutions, such as parsing input, live in `core.clj`
++ Puzzle solutions for each day live in an associated file (`day01.clj`, `day02.clj`, etc.).
+
+## Running
+To run the solution for a given day, use the `lein run` command like so, replacing `01` with the number of the day whose solution you are interested in:
+
+```
+lein trampoline run -m advent-2019.day01
+```
+
+The trampoline makes it run faster. From the [lein docs](https://github.com/technomancy/leiningen/blob/master/doc/TUTORIAL.md#running-code)
+
+> For long-running lein run processes, you may wish to save memory with the higher-order trampoline task, which allows the Leiningen JVM process to exit before launching your project's JVM.
--- src/advent_2019/day01.clj
+++ src/advent_2019/day01.clj
@@ -2,12 +2,16 @@
   (:require [advent-2019.core :refer [get-input]]))
 
 (defn calc-fuel
-  "Fuel required to launch a given module is based on its mass. Specifically, to find the fuel required for a module, take its mass, divide by three, round down, and subtract 2."
+  "Fuel required to launch a given module is based on its mass.
+  Specifically, to find the fuel required for a module,
+  take its mass, divide by three, round down, and subtract 2."
   [mass]
   (-> mass (/ 3) (Math/floor) (int) (- 2)))
 
 (defn calc-total-fuel
-  "For each module mass, calculate its fuel and add it to the total. Then, treat the fuel amount you just calculated as the input mass and repeat the process, continuing until a fuel requirement is zero or negative."
+  "For each module mass, calculate its fuel and add it to the total.
+  Then, treat the fuel amount you just calculated as the input mass and repeat the process,
+  continuing until a fuel requirement is zero or negative."
   [mass]
   (loop [masses [] m mass]
     (let [fuel (calc-fuel m)]
@@ -15,16 +19,18 @@
         (reduce + masses)
         (recur (conj masses fuel) fuel)))))
 
-(def input (get-input "day01.txt" true))  ; Passing true converts input strings to ints
+(defn part1
+  "What is the sum of the fuel requirements for all of the modules on your spacecraft?"
+  [input]
+  (->> input (map calc-fuel) (reduce +)))
 
-; Part 1 Solution
-(comment "What is the sum of the fuel requirements for all of the modules on your spacecraft?")
-(->> input
-     (map calc-fuel)
-     (reduce +))
+(defn part2
+  "What is the sum of the fuel requirements for all of the modules on your spacecraft
+   when also taking into account the mass of the added fuel?"
+  [input]
+  (->> input (map calc-total-fuel) (reduce +)))
 
-; Part 2 Solution
-(comment "What is the sum of the fuel requirements for all of the modules on your spacecraft when also taking into account the mass of the added fuel?")
-(->> input
-     (map calc-total-fuel)
-     (reduce +))
+(defn -main []
+  (let [input (get-input "day01.txt" true)] ; Passing true converts input strings to ints
+    (println "Day 01, Part 1:" (part1 input))
+    (println "Day 01, Part 2:" (part2 input))))

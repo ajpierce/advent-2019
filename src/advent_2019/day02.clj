ec6bbc831f44750808d001c5f469e4ab82088e38
Andrew J. Pierce <andrewp@spaceflightindustries.com>
2019-12-10 07:34:13 -0500

    Day02 part1 initial solution

--- src/advent_2019/day02.clj
+++ src/advent_2019/day02.clj
@@ -2,23 +2,25 @@
   (:gen-class)
   (:require [advent-2019.core :refer [get-input]]))
 
-(defn op1
-  "Adds together numbers read from two positions and stores the result in a third position.
-  The three integers immediately after the opcode tell you these three positions -
-  the first two indicate the positions from which you should read the input values,
-  and the third indicates the position at which the output should be stored."
-  [x]
-  x)
-
-(defn op2
-  "Opcode 2 works exactly like opcode 1, except it multiplies the two inputs instead of adding them."
-  [x]
-  x)
+(defn execute
+  [intcode i op]
+  (let [[_ p1 p2 o] (take 4 (drop i intcode))]
+    (assoc intcode o (op (nth intcode p1) (nth intcode p2)))))
 
 (defn calc
   " To run one, start by looking at the first integer (called position 0). Here, you will find an opcode - either 1, 2, or 99. The opcode indicates what to do; for example, 99 means that the program is finished and should immediately halt. Encountering an unknown opcode means something went wrong."
-  [x]
-  x)
+  [input]
+  (loop [intcode input i 0]
+    (let [op (nth intcode i)]
+      (case op
+        99 intcode
+        1 (recur (execute intcode i +) (+ i 4))
+        2 (recur (execute intcode i *) (+ i 4))
+        intcode))))
+
+(execute [1 0 0 0 99] 0 +)
+
+(take 4 (drop 0 [1 0 0 0 99]))
 
 (defn part1
   "[Take puzzle input and] replace position 1 with the value 12 and replace position 2 with the value 2.

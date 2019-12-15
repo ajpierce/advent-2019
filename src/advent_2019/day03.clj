f433c4faca364ec7d1b5921104a88d073a072414
Andrew J. Pierce <andrewp@spaceflightindustries.com>
2019-12-15 09:59:23 -0500

    Solve day03 part 1

--- src/advent_2019/day03.clj
+++ src/advent_2019/day03.clj
@@ -2,20 +2,40 @@
   (:gen-class)
   (:require [advent-2019.core :refer [get-input]]))
 
-(defn build-circuit [path]
-  (loop [circuit (hash-map)
-         remaining path]))
+(defn parse-segment
+  ([segment] (parse-segment segment [0 0]))
+  ([segment start]
+   (let [direction (subs segment 0 1)
+         length (Integer/parseInt (subs segment 1))
+         [x y] start
+         [dx dy] (case direction
+                   "U" [0 1]
+                   "D" [0 -1]
+                   "R" [1 0]
+                   "L" [-1 0])]
+     (vec (for [i (map inc (range length))]
+            [(+ x (* dx i)) (+ y (* dy i))])))))
 
-(defn parse-seg [seg]
-  (let [direction (subs sec 0 1)
-        length (Integer/parseInt (subs sec 1))]
-    [direction length]))
+(defn build-circuit [wire]
+  (loop [circuit [[0 0]]
+         remaining wire]
+    (if (empty? remaining)
+      (into (sorted-set) (rest circuit))
+      (let [pos (last circuit)
+            segment (first remaining)
+            path (parse-segment segment pos)]
+        (recur (into circuit path) (rest remaining))))))
+
+(defn calc-manhattan [[x y]] (+ (Math/abs x) (Math/abs y)))
 
 (defn part1
   "To fix the circuit, you need to find the intersection point closest to the central port.
   What is the Manhattan distance from the central port to the closest intersection?"
-  [input]
-  input)
+  [[wire1 wire2]]
+  (let [c1 (build-circuit wire1)
+        c2 (build-circuit wire2)
+        intersections (clojure.set/intersection c1 c2)]
+    (apply min (map calc-manhattan intersections))))
 
 (defn part2
   ""
--- test/advent_2019/day03_test.clj
+++ test/advent_2019/day03_test.clj
@@ -2,11 +2,30 @@
   (:require [clojure.test :refer :all]
             [advent-2019.day03 :refer :all]))
 
-(deftest day03
+(deftest day03part1
+  (testing "Segment Parsing"
+    (testing "Going right"
+      (is (= '([1 0] [2 0] [3 0]) (parse-seg "R3")))
+      (is (= '([4 3] [5 3] [6 3]) (parse-seg "R3" [3 3]))))
+    (testing "Going left"
+      (is (= '([-1 0] [-2 0] [-3 0]) (parse-seg "L3")))
+      (is (= '([2 3] [1 3] [0 3]) (parse-seg "L3" [3 3]))))
+    (testing "Going up"
+      (is (= '([0 1] [0 2] [0 3]) (parse-seg "U3")))
+      (is (= '([3 4] [3 5] [3 6]) (parse-seg "U3" [3 3]))))
+    (testing "Going down"
+      (is (= '([0 -1] [0 -2] [0 -3]) (parse-seg "D3")))
+      (is (= '([3 2] [3 1] [3 0]) (parse-seg "D3" [3 3])))))
+  (testing "Circuit Building"
+    (is (= #{[1 0] [2 0] [3 0] [4 0] [5 0]
+             [6 0] [7 0] [8 0] [8 1] [8 2] [8 3]
+             [8 4] [8 5] [7 5] [6 5] [5 5] [4 5]
+             [3 5] [3 4] [3 3] [3 2]} (build-circuit ["R8" "U5" "L5" "D3"]))))
+
   (testing "Crossed Wires"
-    (is (= 6 (calc '(["R8" "U5" "L5" "D3"]
-                     ["U7" "R6" "D4" "L4"]))))
-    (is (= 159 (calc '(["R75" "D30" "R83" "U83" "L12" "D49" "R71" "U7" "L72"]
-                       ["U62" "R66" "U55" "R34" "D71" "R55" "D58" "R83"]))))
-    (is (= 135 (calc '(["R98" "U47" "R26" "D63" "R33" "U87" "L62" "D20" "R33" "U53" "R51"]
-                       ["U98" "R91" "D20" "R16" "D67" "R40" "U7" "R15" "U6" "R7"]))))
+    (is (= 6 (part1 '(["R8" "U5" "L5" "D3"]
+                      ["U7" "R6" "D4" "L4"]))))
+    (is (= 159 (part1 '(["R75" "D30" "R83" "U83" "L12" "D49" "R71" "U7" "L72"]
+                        ["U62" "R66" "U55" "R34" "D71" "R55" "D58" "R83"]))))
+    (is (= 135 (part1 '(["R98" "U47" "R26" "D63" "R33" "U87" "L62" "D20" "R33" "U53" "R51"]
+                        ["U98" "R91" "D20" "R16" "D67" "R40" "U7" "R15" "U6" "R7"]))))))

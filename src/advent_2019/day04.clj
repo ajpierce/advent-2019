02290e7e0c47292723c28cde51766d77728d0e67
Andrew J. Pierce <andrewp@spaceflightindustries.com>
2019-12-18 00:13:18 -0500

    Solve Day04 part 2

--- src/advent_2019/day04.clj
+++ src/advent_2019/day04.clj
@@ -8,29 +8,54 @@
   "Transforms a string or a number into a list of individual digits"
   [number] (map #(Character/digit % 10) (str number)))
 
+(defn contains-repeat-digit? [digits]
+  (->> digits
+       (partition-by identity)
+       (filter (fn [x] (> (count x) 1)))
+       not-empty))
+
+(defn valid-digits? [digits]
+  (and (apply <= digits)
+       (contains-repeat-digit? digits)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Old, naive implementation ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defn valid-number? [number]
   (let [digits (get-digits number)]
-    (and (not (apply distinct? digits))
-         (apply <= digits))))
+    (valid-digits? digits)))
 
-(defn part1-naive [[start end]]
+(defn part1-naive
+  "This one ended up taking ~6.5s on my computer vs. the 1.2s it ended up being with reducers"
+  [[start end]]
   (count (filter valid-number? (range start (inc end)))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn part1-parallel [[start end]]
-  (->> (range start (inc end))
-       vec ; Reducers work 5x faster on vectors
-       (r/filter valid-number?)
-       (r/foldcat)
-       (count)))
+(defn contains-double-digits? [digits]
+  (->> digits
+       (partition-by identity)
+       (filter (fn [x] (= (count x) 2)))
+       not-empty))
 
-(defn part2 [[start end]]
-  (str start " to " end))
+(defn valid-digits??
+  "An Elf just remembered one more important detail:
+  The two adjacent matching digits are not part of a larger group of matching digits."
+  [digits]
+  (and (apply <= digits)
+       (contains-double-digits? digits)))
+
+(defn part1 [digits]
+  (->> digits (r/filter valid-digits?) (r/foldcat) (count)))
+
+(defn part2 [digits]
+  (->> digits (r/filter valid-digits??) (r/foldcat) (count)))
 
 (defn -main []
-  (let [input (->> "day04.txt"
-                   get-input
-                   first
-                   (#(clojure.string/split % #"-"))
-                   (map #(Integer/parseInt %)))]
-    (time (println "Day 04, Part 1, faster" (part1-parallel input)))))
-    ;(println "Day 04, Part 2:" (part2 input))))
+  (let [[start end] (->> "day04.txt" get-input first
+                         (#(clojure.string/split % #"-"))
+                         (map #(Integer/parseInt %)))
+        numbers (vec (range start (inc end)))
+        digits (->> numbers (r/map get-digits))]
+    ;(time (println "Day 04, Part 1, naive" (part1-naive [start end])))
+    (time (println "Day 04, Part 1:" (part1 digits)))
+    (time (println "Day 04, Part 2:" (part2 digits)))))
--- test/advent_2019/day04_test.clj
+++ test/advent_2019/day04_test.clj
@@ -1,6 +1,6 @@
 (ns advent-2019.day04-test
   (:require [clojure.test :refer [deftest testing is]]
-            [advent-2019.day04 :refer [get-digits valid-number?]]))
+            [advent-2019.day04 :refer [get-digits valid-number? contains-double-digits?]]))
 
 (deftest day04-part1-test
   (testing "Digits"
@@ -16,3 +16,12 @@
     (is (= false (valid-number? 554321)))
     (is (= false (valid-number? 987766)))
     (is (= false (valid-number? 123789)))))
+
+(deftest day04-part2-test
+  (testing "Double Digits"
+    (is (= false (contains-double-digits? (get-digits 123789))))
+    (is (= false (contains-double-digits? (get-digits 111789))))
+    (is (= false (contains-double-digits? (get-digits 123444))))
+    (is (= false (contains-double-digits? (get-digits 121212))))
+    (is (= true (contains-double-digits? (get-digits 111122))))
+    (is (= true (contains-double-digits? (get-digits 112233))))))

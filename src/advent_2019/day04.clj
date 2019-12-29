8ac38e08d3bb61cdcfd0be25aacf09454184bb92
Andrew J. Pierce <andrewp@spaceflightindustries.com>
2019-12-28 21:16:32 -0500

    Speed up and clean up Day 04

--- src/advent_2019/day04.clj
+++ src/advent_2019/day04.clj
@@ -1,47 +1,30 @@
 (ns advent-2019.day04
   (:gen-class)
-  (:require
-   [clojure.core.reducers :as r]
-   [advent-2019.core :refer [get-input]]))
+  (:require [clojure.core.reducers :as r]
+            [taoensso.tufte :refer (defnp p profile)]
+            [advent-2019.core :refer [get-input]]))
 
 (defn get-digit [^Character x] (Character/digit x 10))
 
 (defn get-digits
   "Transforms a string or a number into a list of individual digits"
-  [^Integer number] (map get-digit (str number)))
+  [number]
+  (->> number str (map get-digit)))
 
-(defn contains-repeat-digit? [digits]
-  (->> digits
-       (partition-by identity)
-       (filter (fn [x] (> (count x) 1)))
-       empty?
-       not))
-
-(defn valid-digits? [digits]
+(defn valid-digits?  ;; Part 1 filter function
+  [digits]
   (and (apply <= digits)
-       (contains-repeat-digit? digits)))
+       (not (apply distinct? digits))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Old, naive implementation ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defn valid-number? [number]
-  (let [digits (get-digits number)]
-    (valid-digits? digits)))
-
-(defn part1-naive
-  "This one ended up taking ~6.5s on my computer vs. the 1.2s it ended up being with reducers"
-  [[start end]]
-  (count (filter valid-number? (range start (inc end)))))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defn contains-double-digits? [digits]
+(defn contains-double-digits?
+  [digits]
   (->> digits
        (partition-by identity)
        (filter (fn [x] (= (count x) 2)))
        empty?
        not))
 
-(defn valid-digits??
+(defn valid-digits??  ;; Part 2 filter function
   "An Elf just remembered one more important detail:
   The two adjacent matching digits are not part of a larger group of matching digits."
   [digits]
@@ -49,17 +32,22 @@
        (contains-double-digits? digits)))
 
 (defn part1 [digits]
-  (->> digits (r/filter valid-digits?) (r/foldcat) (count)))
+  (->> digits (r/filter valid-digits?) (r/map (constantly 1)) (r/fold +)))
 
 (defn part2 [digits]
-  (->> digits (r/filter valid-digits??) (r/foldcat) (count)))
+  (->> digits (r/filter valid-digits??) (r/map (constantly 1)) (r/fold +)))
 
 (defn -main []
-  (let [[start end] (->> "day04.txt" get-input first
-                         (#(clojure.string/split % #"-"))
-                         (map #(Integer/parseInt %)))
+  (let [[start end] (->> "day04.txt" get-input first (#(clojure.string/split % #"-")) (map #(Integer/parseInt %)))
         numbers (vec (range start (inc end)))
-        digits (->> numbers (r/map get-digits))]
-    ;(time (println "Day 04, Part 1, naive" (part1-naive [start end])))
-    (time (println "Day 04, Part 1:" (part1 digits)))
-    (time (println "Day 04, Part 2:" (part2 digits)))))
+        digits (map get-digits numbers)]
+    (println "Day 04, Part 1:" (part1 digits))
+    (println "Day 04, Part 2:" (part2 digits))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; To pofile, remove #_ and eval block ;;
+#_(let [numbers (range 402328 (inc 864247))
+        digits (map get-digits numbers)]
+    (profile {} (dotimes [_ 20]
+                  (p ::part1 (part1 digits))
+                  (p ::part2 (part2 digits)))))

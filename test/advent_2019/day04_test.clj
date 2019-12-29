aba546260229cc48a5dc50ae21950620a1b00210
Andrew J. Pierce <andrewp@spaceflightindustries.com>
2019-12-28 21:20:36 -0500

    Fix day04 unit tests to use new functions

--- test/advent_2019/day04_test.clj
+++ test/advent_2019/day04_test.clj
@@ -1,21 +1,21 @@
 (ns advent-2019.day04-test
   (:require [clojure.test :refer [deftest testing is]]
-            [advent-2019.day04 :refer [get-digits valid-number? contains-double-digits?]]))
+            [advent-2019.day04 :refer [get-digits valid-digits? contains-double-digits?]]))
 
 (deftest day04-part1-test
   (testing "Digits"
     (is (= '(1 2 3 4) (get-digits 1234)))
     (is (= '(1 2 3 4) (get-digits "1234"))))
   (testing "Valid Numbers"
-    (is (= true (valid-number? 111111)))
-    (is (= true (valid-number? 122345)))
-    (is (= true (valid-number? 111123))))
+    (is (= true (valid-digits? (get-digits 111111))))
+    (is (= true (valid-digits? (get-digits 122345))))
+    (is (= true (valid-digits? (get-digits 111123)))))
   (testing "Invalid Numbers"
-    (is (= false (valid-number? 223450)))
-    (is (= false (valid-number? 223554)))
-    (is (= false (valid-number? 554321)))
-    (is (= false (valid-number? 987766)))
-    (is (= false (valid-number? 123789)))))
+    (is (= false (valid-digits? (get-digits 223450))))
+    (is (= false (valid-digits? (get-digits 223554))))
+    (is (= false (valid-digits? (get-digits 554321))))
+    (is (= false (valid-digits? (get-digits 987766))))
+    (is (= false (valid-digits? (get-digits 123789))))))
 
 (deftest day04-part2-test
   (testing "Double Digits"

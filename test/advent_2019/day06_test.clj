58292fd51ee9d63e30a54ef1d2dc104c2bab604d
Andrew J. Pierce <andrewp@spaceflightindustries.com>
2019-12-30 17:28:45 -0500

    Clean up day 06 tests with def statements

--- test/advent_2019/day06_test.clj
+++ test/advent_2019/day06_test.clj
@@ -7,28 +7,32 @@
                                        get-pairs
                                        split-to-keywords]]))
 
+(def raw-orbit-data ["COM)B" "B)C" "C)D"
+                     "D)E" "E)F" "B)G" "G)H"
+                     "D)I" "E)J" "J)K" "K)L"])
+(def orbit-map (->> raw-orbit-data get-pairs build-orbit-map))
+(def orbit-tree (build-orbit-tree :COM orbit-map))
+
 (deftest day06
   (testing "split-to-keywords"
-    (is (= '(:B :C) (split-to-keywords "B)C")))
+    (is (= '(:B :C) (split-to-keywords "B)C"))))
+
+  (testing "Parsing Orbit Data"
+    (is (= {:COM '(:B), :B '(:G :C),
+            :C '(:D), :D '(:I :E), :E '(:J :F),
+            :G '(:H), :J '(:K), :K '(:L)}
+           (->> raw-orbit-data get-pairs build-orbit-map))))
+
+  (testing "Getting bodies"
+    (is (= '(:G :C) (get-bodies orbit-map :B)))
+    (is (= :L (get-bodies orbit-map :L))))
+
+  (testing "Building orbit tree"
+    (is (= '(:COM (:B (:G (:H)) (:C (:D (:I) (:E (:J (:K (:L))) (:F))))))
+           (build-orbit-tree :COM orbit-map)))
+    (is (= '(:E (:J (:K (:L))) (:F)) (build-orbit-tree :E orbit-map)))
     nil)
-  (let [raw-orbit-data ["COM)B" "B)C" "C)D"
-                        "D)E" "E)F" "B)G" "G)H"
-                        "D)I" "E)J" "J)K" "K)L"]]
-    (testing "Parsing Orbit Data"
-      (is (= {:COM '(:B), :B '(:G :C),
-              :C '(:D), :D '(:I :E), :E '(:J :F),
-              :G '(:H), :J '(:K), :K '(:L)}
-             (->> raw-orbit-data get-pairs build-orbit-map))))
-    (let [orbit-map (->> raw-orbit-data get-pairs build-orbit-map)]
-      (testing "Getting bodies"
-        (is (= '(:G :C) (get-bodies orbit-map :B)))
-        (is (= :L (get-bodies orbit-map :L)))
-        nil)
-      (testing "Building orbit tree"
-        (is (= '(:COM (:B (:G (:H)) (:C (:D (:I) (:E (:J (:K (:L))) (:F))))))
-               (build-orbit-tree :COM orbit-map)))
-        nil)
-      (let [orbit-tree (build-orbit-tree :COM orbit-map)]
-        (testing "Counting orbits"
-          (is (= 42 (count-orbits orbit-tree)))
-          nil)))))
+
+  (testing "Counting orbits"
+    (is (= 42 (count-orbits orbit-tree)))
+    nil))

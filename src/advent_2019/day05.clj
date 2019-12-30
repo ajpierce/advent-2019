68758000a83a4b2b32616f32a405f50936da0894
Andrew J. Pierce <andrewp@spaceflightindustries.com>
2019-12-29 22:58:33 -0500

    Add Day05 part 1 solution

--- src/advent_2019/day05.clj
+++ src/advent_2019/day05.clj
@@ -1,62 +1,81 @@
 (ns advent-2019.day05
   (:gen-class)
   (:require [clojure.string :as s]
-            [taoensso.tufte :refer (defnp p profile)]
-            [advent-2019.core :refer [get-input parse-int-safely]]))
+            [advent-2019.core :refer [get-input parse-int]]))
+
+(defn num-params [opcode]
+  (cond (= "03" opcode) 1
+        (= "04" opcode) 1
+        :else 3))
 
 (def flipjoin
-  "Reverse elements in a seq, join them, and parse as an integer"
-  (comp #(parse-int-safely 0 %) #(s/join "" %) reverse))
+  "Reverse elements in a seq, and join them as a string"
+  (comp #(s/join "" %) reverse))
 
-(flipjoin [1 2 3])
-(flipjoin "")
-(->> "2"
-     s/reverse
-     (split-at 2)
-     (map flipjoin))
+(defn pad
+  "Pads coll to length n with v"
+  [n v coll]
+  (take n (concat coll (repeat v))))
 
 (defn parse-instruction
   "Given an instruction, will return [opcode modes] with proper padding.
-  For example: 102 becomes [02 001]"
+  For example: 102 becomes [02 100]"
   [instruction]
-  (let [[op modes] (->> instruction str
-                        s/reverse
-                        (split-at 2)
-                        (map flipjoin))
-        opcode (format "%02d" op)
-        padding (cond (= "03" opcode) 2
-                      (= "04" opcode) 2
-                      :else 3)]
-    #_(println "opcode" opcode "modes" modes "padding" padding)
-    [opcode (format (str "%0" padding "d") modes)]))
+  (let [[opcode raw-modes] (->> instruction str s/reverse
+                                (pad 5 \0)
+                                (split-at 2)
+                                (map flipjoin))
+        padding (num-params opcode)
+        modes (->> raw-modes reverse (take padding) (s/join ""))]
+    [opcode modes]))
 
-(parse-instruction "2")
+(defn get-val [program [idx mode]]
+  (if (= \1 mode)
+    idx
+    (parse-int (nth program (int idx)))))
 
 (defn calc
-  "Reads the instruction at index idx; returns the resulting program"
-  [program idx]
-  (let [instruction (first (drop idx program))
-        [opcode modes] (->> instruction str
-                            s/reverse
-                            (split-at 2)
-                            (map flipjoin))]
-    (println "instruction:" instruction)
-    (println "opcode:" opcode)
-    (println "modes:" modes)))
-
-(calc [2 255 1 255] 0)
+  "Read the instruction at index idx. Perform the instruction.
+  Return a vector of [new-program, new-index]"
+  ([program]
+   (calc program 0 1))
+  ([program idx]
+   (calc program idx 1))
+  ([program idx input]
+   (let [instruction (first (drop idx program))
+         [opcode modes] (parse-instruction instruction)]
+     (if (= "99" opcode) nil
+         (let [params (->> program
+                           (drop (inc idx))
+                           (split-at (count modes))
+                           first
+                           (map parse-int))
+               values (map #(get-val program %) (partition 2 (interleave params modes)))
+               output (last params)
+               next-idx (inc (count modes))
+               next-prog (case opcode
+                           "01" (assoc program output (apply + (butlast values)))
+                           "02" (assoc program output (apply * (butlast values)))
+                           "03" (assoc program output input)
+                           "04" (do (println (last values)) program)
+                           nil)]
+           [next-prog next-idx])))))
 
 (defn part1
   "After providing 1 to the only input instruction and passing all the tests,
   what diagnostic code does the program produce?"
-  [input]
-  input)
+  [initial-program]
+  (loop [program initial-program idx 0]
+    (let [[p i] (calc program idx)]
+      (if p
+        (recur p (+ idx i))
+        p))))
 
 (defn part2 [input] nil)
 
 (defn -main []
-  (let [input (->> "day05.txt" get-input first (#(clojure.string/split % #",")) (map parse-int-safely) vec)]
+  (let [input (->> "day05.txt" get-input first (#(clojure.string/split % #",")) vec)]
     (time (println "Day 05, Part 1:" (part1 input)))
-    (time (println "Day 05, Part 2:" (part2 input)))))
+    #_(time (println "Day 05, Part 2:" (part2 input)))))
 
 (-main)
--- test/advent_2019/day05_test.clj
+++ test/advent_2019/day05_test.clj
@@ -5,21 +5,23 @@
 (deftest day05
   (testing "instruction parsing"
     (is (= ["02" "000"] (parse-instruction 2)))
-    (is (= ["01" "001"] (parse-instruction 101)))
+    (is (= ["01" "100"] (parse-instruction 101)))
+    (is (= ["01" "001"] (parse-instruction 10001)))
     (is (= ["11" "000"] (parse-instruction 11)))
-    (is (= ["03" "00"] (parse-instruction 3)))
-    (is (= ["03" "00"] (parse-instruction "03")))
-    (is (= ["03" "01"] (parse-instruction 103)))
+    (is (= ["03" "0"] (parse-instruction 3)))
+    (is (= ["03" "0"] (parse-instruction "03")))
+    (is (= ["03" "1"] (parse-instruction 103)))
     (is (= ["02" "101"] (parse-instruction 10102)))
     (is (= ["01" "111"] (parse-instruction 11101)))
     (is (= ["01" "010"] (parse-instruction 1001)))
     (is (= ["13" "000"] (parse-instruction 13))))
 
-  #_(testing "Day 05 Opcodes"
-      (is (= [1002 4 3 4 99] (calc [1002 4 3 4 33]))))
+  (testing "Day 05 Opcodes"
+    (is (= [1002 4 3 4 99] (first (calc [1002 4 3 4 33]))))
+    (is (= [1101 100 -1 4 99] (first (calc [1101 100 -1 4 0])))))
 
-  #_(testing "Day 02 Opcodes"
-      (is (= [2 0 0 0 99] (calc [1 0 0 0 99])))
-      (is (= [1 0 0 2 99] (calc [1 0 0 3 99])))
-      (is (= [2 3 0 6 99] (calc [2 3 0 3 99])))
-      (is (= [2 4 4 5 99 9801] (calc [2 4 4 5 99 0])))))
+  (testing "Day 02 Opcodes"
+    (is (= [2 0 0 0 99] (first (calc [1 0 0 0 99]))))
+    (is (= [1 0 0 2 99] (first (calc [1 0 0 3 99]))))
+    (is (= [2 3 0 6 99] (first (calc [2 3 0 3 99]))))
+    (is (= [2 4 4 5 99 9801] (first (calc [2 4 4 5 99 0]))))))
